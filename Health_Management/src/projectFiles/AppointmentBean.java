9b9a2a61b5c0d5636e3728308852f28d6c8de364
paulsoriiiano <53189056+paulsoriiiano@users.noreply.github.com>
2020-12-04 13:48:58 -0800

    beans

---
+++ Health_Management/src/projectFiles/AppointmentBean.java
@@ -0,0 +1,31 @@
+package projectFiles;
+
+public class AppointmentBean {
+    String appName, timing, notes;
+
+    //Getters
+    public String getAppName() {
+        return appName;
+    }
+
+    public String getTiming() {
+        return timing;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    //Setters
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public void setTiming(String timing) {
+        this.timing = timing;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
---
+++ Health_Management/src/projectFiles/MedicationBean.java
@@ -0,0 +1,49 @@
+package projectFiles;
+
+public class MedicationBean {
+    String medicationName, dose, time, notes;
+    String id;
+    //Getters
+
+    public String getID() {
+        return id;
+    }
+
+    public String getMedicationName() {
+        return medicationName;
+    }
+
+    public String getDose() {
+        return dose;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    //Setters
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public void setMedicationName(String medicationName) {
+        this.medicationName = medicationName;
+    }
+
+    public void setDose(String dose) {
+        this.dose = dose;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
