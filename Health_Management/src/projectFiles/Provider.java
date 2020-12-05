9120e5fd2bc11700a7630f9ead2dbbd9e71fa6a7
surab <surabhi2029@gmail.com>
2020-12-05 12:45:13 -0800

    Removes unnecessary files

--- Health_Management/src/projectFiles/Provider.java
+++
@@ -1,9 +0,0 @@
-package projectFiles;
-
-public interface Provider {  
-String DRIVER="oracle.jdbc.driver.OracleDriver";  
-String CONNECTION_URL="jdbc:oracle:thin:@localhost:3001:xe";  
-String USERNAME="system";  
-String PASSWORD="oracle";  
-  
-}  
